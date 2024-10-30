a48e8a77d9d374a059d86d56ec45e5b00380e833
João Lucas Lourenço <jao.lucas.lourenco@gmail.com>
2024-10-30 10:21:19 -0300

    property binding

---
+++ curso-angular/src/app/componente03/componente03.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-componente03',
+  standalone: true,
+  imports: [],
+  templateUrl: './componente03.component.html',
+  styleUrl: './componente03.component.css'
+})
+export class Componente03Component {
+  imagem:string = "assets/anao.jfif"
+  alterarImagem(){
+    if(this.imagem === "assets/anao.jfif"){
+      this.imagem = "assets/elfo.jfif"
+    }
+    else{
+      this.imagem = "assets/anao.jfif"
+    }
+  }
+}
