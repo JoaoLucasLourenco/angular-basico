5485d335c26c31a67ffe009025c3046e713a880c
João Lucas Lourenço <jao.lucas.lourenco@gmail.com>
2024-11-06 14:22:31 -0300

    projeto finalizado

--- curso-angular/src/app/componente11/componente11.component.ts
+++ curso-angular/src/app/componente11/componente11.component.ts
@@ -1,12 +1,74 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Pessoa } from '../model/Pessoa';
 
 @Component({
   selector: 'app-componente11',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './componente11.component.html',
   styleUrl: './componente11.component.css'
 })
-export class Componente11Component {
 
+export class Componente11Component {
+  indice:number = -1;
+  btnCadastrar:boolean = true
+  formulario = new FormGroup({
+    nome: new FormControl('', [Validators.required, Validators.minLength(3)]),
+    idade: new FormControl(null,[Validators.required,Validators.min(0),Validators.max(120)]),
+    cidade: new FormControl('',[Validators.required, Validators.minLength(3)])
+  })
+
+  vetor:Pessoa[] = []
+  
+  cadastrar(){
+    this.vetor.push(this.formulario.value as Pessoa)
+    this.formulario.reset();
+  }
+
+  selecionar(indice:number){
+    this.indice = indice
+
+    this.formulario.setValue({
+      nome: this.vetor[indice].nome,
+      idade: this.vetor[indice].idade,
+      cidade: this.vetor[indice].cidade
+    })
+    this.btnCadastrar = false
+  }
+
+  alterar(){
+
+    this.vetor[this.indice] = this.formulario.value as Pessoa;
+
+    this.formulario.reset();
+
+    this.btnCadastrar = true;
+
+  }
+
+  remover(){
+
+    // Removendo pessoa do vetor
+    this.vetor.splice(this.indice, 1);
+
+    // Limpeza dos inputs
+    this.formulario.reset();
+
+    // Visibilidade dos botões
+    this.btnCadastrar = true;
+
+  }
+
+
+  cancelar(){
+
+    // Limpeza dos inputs
+    this.formulario.reset();
+
+    // Visibilidade dos botões
+    this.btnCadastrar = true;
+
+  }
 }
